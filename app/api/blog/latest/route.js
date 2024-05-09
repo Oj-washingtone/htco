c80de3ac5017582823cfa18218510886a9bf7b37
JALANGO WASHINGTONE <jalangowashingtone@gmail.com>
2024-05-09 21:45:29 +0300

    Docker

--- app/api/blog/latest/route.js
+++ app/api/blog/latest/route.js
@@ -5,6 +5,6 @@ import BlogPost from "@/models/BlogPost";
 
 export async function GET() {
   // get latest 2 posts
-  const posts = await BlogPost.find().sort({ createdAt: -1 }).limit(2);
-  return NextResponse.json(posts);
+  // const posts = await BlogPost.find().sort({ createdAt: -1 }).limit(2);
+  return NextResponse.json("posts");
 }
